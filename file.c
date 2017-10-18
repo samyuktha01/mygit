185f85e483b2a7e1b7bdf3cb713231bfb9204034
samyuktha01 <samyukthadsr112@gmail.com>
2017-10-18 21:50:43 +0530

    updated c program

--- file.c
+++ file.c
@@ -1,1 +1,13 @@
-this is my c file
+#this is my c file
+
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int a=10;
+	int b=20;
+	int c=a+b;
+	printf("the sum of a and b is %d",c);
+	scanf("%d",&c);
+	clrscr();
+}
